686d06f3f5c0c6ddbdbb2a5fb1e32883f7b7869c
Nanji Lakan <86968487+Shaelle11@users.noreply.github.com>
2024-01-27 06:59:36 +0100

    added credentials in header

--- index.js
+++ index.js
@@ -32,8 +32,6 @@ app.use(cors(corsOptions));
 // Explicitly set the Access-Control-Allow-Origin header
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', allowedOrigins);
-  // ... other headers
-  next();
 });
 
 app.use(express.json());
