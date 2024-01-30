8553f7152a8ff38acd7986057d02e94e8ffec0e1
Nanji Lakan <86968487+Shaelle11@users.noreply.github.com>
2024-01-30 13:47:48 +0100

    minor changes

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ const corsOptions = {
    methods: ["POST", "GET", "PUT", "DELETE"],
    credentials: true// some legacy browsers (IE11, various SmartTVs) choke on 204
   };
-  
+       
   app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser()); 
